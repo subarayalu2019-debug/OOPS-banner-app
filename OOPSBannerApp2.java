1ca03efde1185301425037cf8ea22976576d2064
pranav <subarayalu2019@gmail.com>
2026-03-01 14:36:59 +0000

    Coded UC2

---
+++ OOPSBannerApp2.java
@@ -0,0 +1,14 @@
+public class OOPSBannerApp2 {
+    public static void main(String[] args) {
+
+
+        System.out.println("  *****  " + "  *****  " + "  *****  " + "  *****  ");
+        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **   ** ");
+        System.out.println("**     **" + "**     **" + "**     **" + "**      ");
+        System.out.println("**     **" + "**     **" + "*******  " + "  *****  ");
+        System.out.println("**     **" + "**     **" + "**       " + "      ** ");
+        System.out.println(" **   ** " + " **   ** " + "**       " + " **   ** ");
+        System.out.println("  *****  " + "  *****  " + "**       " + "  *****  ");
+
+    }
+}
