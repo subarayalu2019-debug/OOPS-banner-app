51989b5e41d46f699e8065a72c47d320edc9fb21
pranav <subarayalu2019@gmail.com>
2026-03-01 15:25:30 +0000

    Coded UC3

---
+++ OOPSBannerApp3.java
@@ -0,0 +1,49 @@
+public class OOPSBannerApp3 {
+
+    public static void main(String[] args) {
+
+        // Each letter is 7 lines high
+
+        System.out.println(String.join("",
+                "  **  ",
+                "  **  ",
+                "  **  ",
+                "  **  "));
+
+        System.out.println(String.join("",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * "));
+
+        System.out.println(String.join("",
+                "*     *",
+                "*     *",
+                "*     *",
+                "**      "));
+
+        System.out.println(String.join("",
+                "*     *",
+                "*     *",
+                "***  ",
+                "  ***  "));
+
+        System.out.println(String.join("",
+                "*     *",
+                "*     *",
+                "*       ",
+                "      * "));
+
+        System.out.println(String.join("",
+                " *   * ",
+                " *   * ",
+                "*       ",
+                " *   ** "));
+
+        System.out.println(String.join("",
+                "  **  ",
+                "  **  ",
+                "*       ",
+                "  **  "));
+    }
+}
